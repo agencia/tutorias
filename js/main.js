6cb4d73c2951b54e72c01f9d5757116e2f80cf6a
Fernando Medrano <fernando.medrano@upa.edu.mx>
2014-08-17 16:06:02 -0500

    login

--- js/main.js
+++ js/main.js
@@ -1,1 +1,37 @@
 
+window.TUTORIAS = {
+	site: "tutorias.medrano.in",
+	views: {},
+	token: null
+}
+
+TUTORIAS.views.Layout = Backbone.View.extend({
+
+});
+
+TUTORIAS.views.Login = Backbone.View.extend({
+	el : $("#app"),
+	template :  _.template($("login-template").html()),
+	events: {
+	  	"submit #form-login" : "login"
+	  },
+	render: function(){
+		this.$el.html(this.template);
+  		return this;
+	},
+
+	login: function(event){
+	  	event.preventDefault();
+	  	TUTORIAS.token = 'xWZmin';
+	  }
+
+});
+
+
+$(function(){
+	if(TUTORIAS.token){
+		TUTORIAS.app = new TUTORIAS.views.Layout();
+	} else {
+		TUTORIAS.app = new TUTORIAS.views.Login();
+	}
+});
